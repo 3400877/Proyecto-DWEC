95aea0ab53ef2ad539f783b22a8d69db8ccf3db2
Adrián <3400877@gmail.com>
2023-12-11 10:14:56 +0100

    Testing

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const actualDay = document.getElementById("actual-day");
 const yesterday = document.getElementById("yesterday");
 const tomorrow = document.getElementById("tomorrow");
 const days = document.getElementsByClassName("day");
-const movieListHeader = document.getElementById("h_movies")
+const movieListHeader = document.getElementById("h_movies");
 // Navigation bar stuff
 const addMovieButton = document.getElementById("add-movie");
 const addMoviePopup = document.getElementById("movie-popup");
+
 
 const monthText = [
 	'January', 'February', 'March', 'April',
@@ -102,13 +103,14 @@ class Calendar {
 
 	updateDay = () => {
 		[...days].forEach((day, index) => {
-			if (day.textContent == this.day ) {
+			if (day.textContent == this.day) {
 				days.item(index).setAttribute("class", ACTUAL);
 			} else if (day.getAttribute("class") == ACTUAL) {
 				days.item(index).setAttribute("class", DAY);
 			}
 		});
 		this.printDay(actualDay);
+		dispatchEvent(new Event('dateUpdated'));
 	}
 
 	static appendDay = (day, actual) => {
@@ -121,17 +123,17 @@ class Calendar {
 		dayElement.textContent = day;
 		return dayElement;
 	}
-	
+
 	getDate = () => {
 		return new Date(this.year, this.month, this.day);
 	}
 
 	bindControls = () => {
-		yesterday.onclick = () => this.decreaseDay();
+		yesterday.onclick = () => this.reduceDay();
 		tomorrow.onclick = () => this.increaseDay();
 		calendar.addEventListener("click", (day) => {
-			if (day.target && day.target.tagName == "TIME") {
-				this.setDay(day.target.textContent);
+			if (day.target && day.target.tagName == "TIME" && day.target.textContent.length != 0) {
+				this.setDay(parseInt(day.target.textContent));
 			}
 		})
 	}
@@ -139,35 +141,115 @@ class Calendar {
 
 class MovieList {
 	constructor(calendar) {
-		this.list = [];
+		this.movieList = [];
 		this.calendar = calendar;
 		this.printDate();
+		addEventListener("dateUpdated", () => {
+			this.printDate();
+			this.addMovieListArticle();
+		});
 	}
 
 	addMovie = (movie) => {
-		this.list.push(movie);
+		this.movieList.push(movie);
 	}
-	
+
 	addMovieJSON = (movieJSON) => {
-		this.addMovies(...JSON.parse(movieJSON));
+		this.addMovies(JSON.parse(movieJSON).movies);
 	}
-	
-	addMovies = (...movies) => {
-		movies.forEach((movie) => this.list.push(movie));
-	}
-	
+
+	addMovies = (movies) => movies.forEach(this.addMovie);
+
 	printDate = () => this.calendar.printDay(movieListHeader);
-}
 
-class Movie {
-	constructor(title, year) {
-		this.title = title;
-		this.year = year;
+	addMovieListArticle = () => {
+		// Create a new article for the list of movies
+		const movieListArticle = document.createElement('article');
+
+		this.movieList.forEach(movie => {
+			// Create a div for each movie
+			const movieDiv = document.createElement('div');
+			movieDiv.setAttribute("class", "movie bordered");
+
+			// Add the title of the movie
+			const movieTitle = document.createElement('h2');
+			movieTitle.textContent = movie.title;
+			movieDiv.appendChild(movieTitle);
+
+			// Add the movie year to the div
+			const movieYear = document.createElement('p');
+			movieYear.textContent = `Year: ${movie.year}`;
+			movieDiv.appendChild(movieYear);
+
+			// Add the movie director to the div
+			const movieDirector = document.createElement('p');
+			movieDirector.textContent = `Director: ${movie.director}`;
+			movieDiv.appendChild(movieDirector);
+
+			// Create a new div for the cast
+			const castDiv = document.createElement('div');
+
+			// Loop through each actor in the cast array
+			movie.cast.forEach(actor => {
+				// Add the actor to the cast div
+				const castMember = document.createElement('p');
+				castMember.textContent = actor;
+				castDiv.appendChild(castMember);
+			});
+
+			// Add the cast div to the movie div
+			movieDiv.appendChild(castDiv);
+
+			// Add the movie div to the article
+			movieListArticle.appendChild(movieDiv);
+		})
+		movieListHeader.parentNode.replaceChild(movieListArticle, movieListHeader.nextSibling);
 	}
 }
 
-const calendar1 = new Calendar(11, 2023, 29);
+const movieJsonString = `{
+    "movies": [
+        {
+            "title": "Silent Night",
+            "year": 2023,
+			"month": 11,
+			"day": 30,
+            "director": "Camille Griffin",
+            "cast": [
+                "Keira Knightley",
+                "Lily-Rose Depp",
+                "Matthew Goode"
+            ]
+        },
+        {
+            "title": "Candy Cane Lane",
+            "year": 2023,
+			"month": 10,
+			"day": 1,
+            "director": "Maggie Carey",
+            "cast": [
+                "Emma Stone",
+                "Ryan Gosling",
+                "Zoey Deutch"
+            ]
+        },
+        {
+            "title": "Renaissance: A Film By Beyoncé",
+            "year": 2023,
+			"month": 12,
+			"day": 21,
+            "director": "Beyoncé and Ed Burke",
+            "cast": []
+        }
+    ]
+}`;
+
+const actualDate = new Date();
+
+const calendar1 = new Calendar(actualDate.getMonth() + 1, actualDate.getFullYear(), actualDate.getDate());
 const movieList1 = new MovieList(calendar1);
+movieList1.addMovieJSON(movieJsonString);
+movieList1.addMovieListArticle();
 
 addMovieButton.onclick = () => addMoviePopup.style.display = addMoviePopup.style.display == 'none'
 	? 'flex'
